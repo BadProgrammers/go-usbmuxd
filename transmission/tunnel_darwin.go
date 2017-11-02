1a813305e6aa6fd22c86a1330b65f2afbb895879
Ameya <ameya@helpshift.com>
2017-11-02 13:18:35 +0530

    Code cleanup, removing hardcoding, commenting

--- transmission/tunnel_darwin.go
+++ transmission/tunnel_darwin.go
@@ -1,22 +1,19 @@
 // +build darwin dragonfly freebsd linux nacl netbsd openbsd solaris
+
 package transmission
 
 import (
 	"net"
 )
 
+var osFamily = "unix"
+var tcpFile = "/var/run/usbmuxd"
+
+// Tunnel USB function for Unix family
 func Tunnel() (net.Conn, error) {
-	conn, err := net.Dial("unix", "/var/run/usbmuxd")
+	conn, err := net.Dial(osFamily, tcpFile)
 	if err != nil {
 		return nil, err
 	}
 	return conn, nil
 }
-
-
-
-
-
-
-
-
