3813c21f8337161ca50473a72f4f3abb8ff48211
Ameya <ameya@helpshift.com>
2017-11-06 12:58:36 +0530

    Adding device structure and array with containig package.

---
+++ devices/devices.go
@@ -0,0 +1,9 @@
+package devices
+
+// Device - A structure containing fields related to the device.
+type Device struct {
+	deviceID string
+}
+
+// ConnectedDevices - Array containing all the connected devices.
+var ConnectedDevices []Device
